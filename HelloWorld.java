8cf2967edead3079d0671adfc4ab1e341b31b4ae
Amit Indoria <x19195249@student.ncirl.ie>
2021-01-06 11:21:11 +0530

    Master Commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,8 +5,8 @@ public class HelloWorld {
 
        for (int i=0; i<=10; i++) 
       {
-       
-       System.out.println(" Dev Branching");
+       System.out.println("Hello World");
+      
        
       }
     }
