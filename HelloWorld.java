fe1d1c33ef5e1a3f10acb197c536cc46f0e63af8
Amit Indoria <x19195249@student.ncirl.ie>
2021-01-06 10:59:53 +0530

    Dev Branch Modified

--- HelloWorld.java
+++ HelloWorld.java
@@ -6,7 +6,7 @@ public class HelloWorld {
        for (int i=0; i<=10; i++) 
       {
        System.out.println(" Hello World");
-       System.out.println(" Checking Commit");
+       System.out.println(" Dev Branch");
       }
     }
 }
